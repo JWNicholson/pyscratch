84775b5aba1edd3200f961d02a751d5fd6b8fa5d
JWNicholson <john.nicholson812@gmail.com>
2020-07-20 09:40:35 -0400

    started working with strings

---
+++ headscratch.py
@@ -0,0 +1,18 @@
+## strings - Indexing & Slicing#############
+
+s = 'Whatnowhomer?'
+
+# print 5th element
+print(s[4])
+
+#print last element when unkonwn length
+print(s[-1])
+
+
+## Slicing ##
+
+#start at index 2, go to the end
+print(s[2:])
+
+#start at beginning and go to 4th index
+print(s[:4])
