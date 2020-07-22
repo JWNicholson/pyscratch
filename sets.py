8fd854f1a7306dfff1e8d8160d22a0d1ebcb2de7
JWNicholson <john.nicholson812@gmail.com>
2020-07-22 10:48:15 -0400

    added sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+"""
+    Sets are unordered collenctions of unuque elements. 
+    - there can only be one representative of the same object
+"""
+
+myset = set()
+
+## add object
+myset.add(1)
+print(myset)
+
+myset.add(2)
+print(myset)
+
