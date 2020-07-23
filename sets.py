9821981dae8226c8ddcc431c808359a5774012ad
JWNicholson <john.nicholson812@gmail.com>
2020-07-23 09:06:50 -0400

    setup for input/output basics with files

--- sets.py
+++ sets.py
@@ -3,12 +3,12 @@
     - there can only be one representative of the same object
 """
 
-myset = set()
-
+myset = set([1,1,2,3])
+print(myset)
 ## add object
-myset.add(1)
-print(myset)
+# myset.add(1)
+# print(myset)
 
-myset.add(2)
-print(myset)
+# myset.add(2)
+# print(myset)
 
