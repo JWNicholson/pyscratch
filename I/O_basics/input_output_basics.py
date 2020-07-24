bdf592d725eaaf5e9af5682a2dbac84b55dbb14c
JWNicholson <john.nicholson812@gmail.com>
2020-07-24 15:16:48 -0400

    updated method to read and close files (with)

--- I/O_basics/input_output_basics.py
+++ I/O_basics/input_output_basics.py
@@ -1,21 +1,35 @@
-f = open('test.txt', 'r')
+ #f = open('test.txt', 'r')
+#g = open('/mnt/h/1.PersonalProjs/pyscratch/sometext.txt')
 
 
+# print(f.read())
 
-print(f.read())
+# print("++++++++++++++++++++++++")
+# # in order to read the file again, set the cursor back to zero
+# # otherwise you get a blank return
+# f.seek(0)
 
-print("++++++++++++++++++++++++")
-# in order to read the file again, set the cursor back to zero
-# otherwise you get a blank return
-f.seek(0)
+# print(f.read())
 
-print(f.read())
+# print('**************')
 
-print('**************')
+# # return as a list
+# f.seek(0)
+# print(f.readlines())
+# f.close()
 
-# return as a list
-f.seek(0)
-print(f.readlines())
+# print(g.read())
+
+"""
+    New and better way - combines read and close functions
+"""
 
 
+with open('/mnt/h/1.PersonalProjs/pyscratch/sometext.txt') as g_file:
+    contents = g_file.read()
+    print(contents)
+
+with open('test.txt') as horseFace:
+    horse = horseFace.read()
+    print(horse)
 
