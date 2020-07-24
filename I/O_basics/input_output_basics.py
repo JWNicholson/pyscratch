0d6e8dcb36a3032a4cf6e33b2aa8948e7fc416cf
JWNicholson <john.nicholson812@gmail.com>
2020-07-24 15:38:45 -0400

    finished with input_output file basics for now

--- I/O_basics/input_output_basics.py
+++ I/O_basics/input_output_basics.py
@@ -25,11 +25,35 @@
 """
 
 
-with open('/mnt/h/1.PersonalProjs/pyscratch/sometext.txt') as g_file:
-    contents = g_file.read()
-    print(contents)
+# with open('/mnt/h/1.PersonalProjs/pyscratch/sometext.txt') as g_file:
+#     contents = g_file.read()
+#     print(contents)
 
-with open('test.txt') as horseFace:
-    horse = horseFace.read()
-    print(horse)
+# with open('test.txt') as horseFace:
+#     horse = horseFace.read()
+#     print(horse)
+"""
+r = read
+w = write only (overwrites files or creates new one if it doesn't exist)
+a = append
+r+ = read and write
+w+ = writing and reading (Overwrites existing file or creates a new file if it doesn't exist)
+"""
+# write in to file
+with open('readwrite.txt', mode='r') as f:
+    print(f.read())
 
+
+#append
+with open('readwrite.txt', mode='a') as f:
+    f.write('\nLater Gator')
+
+with open('readwrite.txt', mode='r') as f:
+    print(f.read())
+
+# create cause it dont exists yet
+with open('giggedy.txt', mode='w') as f:
+    f.write('Quagmire')
+
+with open('giggedy.txt',mode='r') as f:
+    print(f.read())
