f90a9885788bc2de6bc0d83b7e2590c3e245f78b
JWNicholson <john.nicholson812@gmail.com>
2020-08-10 11:08:13 -0400

    working on FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,25 @@
+# Need to count 1 to 100 and print each int, except-
+# if int is a multiple of 3 print "Fizz",
+# if int is a multiple of 5 print "Buzz"'
+#if int is a multiple of 5 and 3 print "FizzBuzz"
+
+#Need to set a variable to hold the current number
+#Need for loop to itereate throught the number range
+#Check if number modulus 3 and 5 are 0 , if yes print "FizzBuzz-DO THIS CHECK FIRST AND CONTINUE, otherwise value errors will occur
+#Check if number modulus 3 = 0 - if yes print "Fizz"
+#Check if number modulus 5 = 0 - if yes print "Buzz"
+#Increment the current number by 1
+
+#initialize variable to hold count
+#num = 0
+x = range(1,101)
+
+for n in x:
+    #if n modulo 3 and 5 == 0
+
+    #if n modulo 3 == 0
+
+    #if n modulo 5 == 0
+
+    #else print n
+    print(n)
